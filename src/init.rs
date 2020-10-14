d1b2ea3ffc4feaada4e71e4098bec74db96bd28d
Noah <33094578+coolreader18@users.noreply.github.com>
2020-10-14 00:04:43 -0500

    Fix compilation on 1.46

--- src/init.rs
+++ src/init.rs
@@ -12,21 +12,25 @@ pub fn pyckitup_init(project_name: PathBuf) -> anyhow::Result<()> {
         project_name.display()
     );
     std::fs::create_dir_all(project_name.join("static"))?;
+    fn bytesref(x: &[u8]) -> &[u8] {
+        // hack to coerce to &[u8]
+        x
+    }
     std::fs::write(
         project_name.join("static/click.wav"),
-        include_bytes!("../static/click.wav"),
+        bytesref(include_bytes!("../static/click.wav")),
     )?;
     std::fs::write(
         project_name.join("run.py"),
-        include_bytes!("../examples/clock.py"),
+        bytesref(include_bytes!("../examples/clock.py")),
     )?;
     std::fs::write(
         project_name.join("common.py"),
-        include_bytes!("../examples/common.py"),
+        bytesref(include_bytes!("../examples/common.py")),
     )?;
     std::fs::write(
         project_name.join(".gitignore"),
-        include_bytes!("../include/gitignore"),
+        bytesref(include_bytes!("../include/gitignore")),
     )?;
     println!("Initialized. To run: `pyckitup run`");
 
